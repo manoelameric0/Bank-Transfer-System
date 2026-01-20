1879a0cc678ff2e444f2b8e2b2e4ee37c547b8b4
JoaoMendes-A <JoaomenndesArj@hotmail.com>
2026-01-20 18:07:25 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Exercicio
+{
+    class ContaBancaria
+    {
+        public string Titular { get; set; }
+        private int Numero;
+        private double Saldo;
+
+        public ContaBancaria(string titular, int numero, double saldo)
+        {
+            Titular = titular;
+            Numero = numero;
+            Saldo = saldo;
+        }
+
+        public ContaBancaria()
+        {
+            Titular = "";
+            Numero = 0;
+            Saldo = 0;
+        }
+
+    }
+
+    static void Main()
+    {
+        Console.WriteLine();
+    }
+
+    public static ContaBancaria CriarConta(string nome, int numero)
+    {
+        return new ContaBancaria(nome, numero, 0);
+    }
+
+    public static int GerarNumero()
+    {
+        Random rnd = new Random();
+        int numero = rnd.Next(100, 199);
+        return numero;
+    }
+}
